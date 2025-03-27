f88270b1a93dc96b296b78e9ea63e06127c95da7
SteveIpartek <estebansuarezramos5@gmail.com>
2025-03-27 20:52:14 +0100

    Ver 4.0 optimizacion + correciones

--- js/script.js
+++ js/script.js
@@ -1,42 +1,26 @@
-//Duda se puede hacer que cuando entres por primera vez
+let visitas = localStorage.getItem("visitas");// Obtiene el valor de "visitas" del almacenamiento local del navegador.
+// Obtiene una referencia al elemento HTML con el ID "contadorVisitas".
+const capNumVis = document.getElementById("contadorVisitas");// Este elemento muestra el número de visitas.
+const reset = document.getElementById("btnReestablecer");// Obtiene una referencia al botón de reinicio con el ID "btnReestablecer".
 
+function program (){//Funcion Principal del programa.
+    visitas ++;// Incrementa el contador de visitas en 1.
+    localStorage.setItem("visitas", visitas);// Guarda el nuevo valor de "visitas" en el almacenamiento local.
+    capNumVis.textContent = visitas;// Actualiza el contenido de texto del elemento con el número de visitas actual.
+    }
 
+if (isNaN(visitas)) {// Verifica si el valor obtenido no es un número (NaN).
 
-// Obtiene el valor de "visitas" del almacenamiento local del navegador.
-let visitas = localStorage.getItem("visitas");
-
-// Verifica si el valor obtenido no es un número (NaN).
-if (isNaN(visitas)) {
-    // Si no es un número, significa que el valor es inválido.
-    // Inicializa "visitas" a 0.
-    visitas = 0;
+    visitas = 0;// Inicializa "visitas" a 0.
 } else {
-    // Si es un número, lo convierte a un entero.
-    visitas = parseInt(visitas);
+    
+    visitas = parseInt(visitas);// Si es un número, lo convierte a un entero.
+    program();
 }
 
-// Incrementa el contador de visitas en 1.
-visitas += 1;
-
-// Guarda el nuevo valor de "visitas" en el almacenamiento local.
-localStorage.setItem("visitas", visitas);
-
-// Obtiene una referencia al elemento HTML con el ID "contadorVisitas".
-// Este elemento muestra el número de visitas.
-const capNumVis = document.getElementById("contadorVisitas");
-
-// Actualiza el contenido de texto del elemento con el número de visitas actual.
-capNumVis.textContent = visitas;
-
-// Obtiene una referencia al botón de reinicio con el ID "btnReestablecer".
-const reset = document.getElementById("btnReestablecer");
-
 // Agrega un escuchador de eventos al botón de reinicio.
-// Cuando se hace clic en el botón, se ejecuta la función siguiente.
-reset.addEventListener("click", function() {
-    // Reinicia el valor de "visitas" en el almacenamiento local a 0.
-    localStorage.setItem("visitas", 0);
-
-    // Actualiza el contenido de texto del elemento "contadorVisitas" a 0.
-    capNumVis.textContent = 0;
+reset.addEventListener("click", function() {// Cuando se hace clic en el botón, se ejecuta la función siguiente.
+    
+    localStorage.setItem("visitas", 0);// Reinicia el valor de "visitas" en el almacenamiento local a 0.
+    capNumVis.textContent = 0;// Actualiza el contenido de texto del elemento "contadorVisitas" a 0.
 });
